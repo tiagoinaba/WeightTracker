177e08bfcd680a9f511e9e44fb8ad1c6d62a4d51
tiagoinaba <tiago.inaba@gmail.com>
2023-02-06 22:41:06 -0300

    added kg or lbs option

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const weightTable = document.getElementById('weight-table')
 const addBtn = document.getElementById('add-btn')
 const weightInput = document.getElementById('weight-input')
 const suffixElement = document.getElementById('my-suffix')
+const checkbox = document.getElementById('checkbox')
 let today = new Date()
 let dd = String(today.getDate()).padStart(2, '0')
 let mm = String(today.getMonth() + 1).padStart(2, '0')
 let yyyy = today.getFullYear()
 let weighIns = new Array()
+let isKg = checkbox.checked
 
 today = `${dd}/${mm}/${yyyy}`
 let todayNumbersOnly = dd + mm + yyyy
@@ -41,27 +43,68 @@ function sortLocalStorage(){
 }
 var sortedLocalStorage = sortLocalStorage()
 
+checkbox.addEventListener('change', kgsOrLbs)
 
-
-
-let htmlString = ""
-
-for(let i=0; i < sortedLocalStorage.length; i++) {
-    let item = sortedLocalStorage[i]
-    htmlString = htmlString + `<tr>
-    <td id="${item.localStorageKey}">${item.data.date}</td>
-    <td id="${item.localStorageKey}-weight">${item.data.weightInKg}kg</td>
-</tr>`
-    weighIns[i] = {
-        date: item.data.date,
-        weightInKg: item.data.weightInKg,
-        weightInLbs: item.data.weightInLbs
+function kgsOrLbs() {
+    if(checkbox.checked) {
+        weightInput.placeholder = 'Input weight in kg'
+        suffixElement.innerText = 'kg'
+        isKg = true
+        renderAverages()
+        renderWeighIns()
+    } else {
+        weightInput.placeholder = 'Input weight in lbs'
+        suffixElement.innerText = 'lbs'
+        isKg = false
+        renderAverages()
+        renderWeighIns()
     }
 }
-weighIns.reverse()
-weightTable.innerHTML += htmlString;
 
+
+
+function renderWeighIns() {
+    let htmlString = ""
+    resetWeighInsTable()
+    if(isKg) {
+        sortedLocalStorage = sortLocalStorage()
+        for(let i=0; i < sortedLocalStorage.length; i++) {
+            let item = sortedLocalStorage[i]
+            htmlString = htmlString + `<tr>
+            <td id="${item.localStorageKey}">${item.data.date}</td>
+            <td id="${item.localStorageKey}-weight">${item.data.weightInKg}kg</td>
+            </tr>`
+            weighIns[i] = {
+                date: item.data.date,
+                weightInKg: item.data.weightInKg,
+                weightInLbs: item.data.weightInLbs
+            }
+        }
+    } else {
+        sortedLocalStorage = sortLocalStorage()
+        for(let i=0; i < sortedLocalStorage.length; i++) {
+            let item = sortedLocalStorage[i]
+            htmlString = htmlString + `<tr>
+            <td id="${item.localStorageKey}">${item.data.date}</td>
+            <td id="${item.localStorageKey}-weight">${item.data.weightInLbs}lbs</td>
+            </tr>`
+            weighIns[i] = {
+                date: item.data.date,
+                weightInKg: item.data.weightInKg,
+                weightInLbs: item.data.weightInLbs
+            }
+        }
+    }
+    weightTable.innerHTML += htmlString;
+}
+
+
+
+weighIns.reverse()
+
+renderWeighIns()
 renderAverages()
+
 
 weightInput.addEventListener('input', function() {
     if(weightInput.value === ''){ 
@@ -93,29 +136,57 @@ weightInput.onkeydown = function(e) {
 addBtn.addEventListener('click', function() {
 
     if (weightInput.value) {
-        let data = {
-            date: today,
-            weightInKg: weightInput.value,
-            weightInLbs: (weightInput.value * 2.20).toFixed(2)
-        }
+        if(isKg) {
+            let data = {
+                date: today,
+                weightInKg: weightInput.value,
+                weightInLbs: (weightInput.value * 2.20462262185).toFixed(2)
+            }
+
+            localStorage.setItem(`${todayNumbersOnly}`, JSON.stringify(data))
         
-        localStorage.setItem(`${todayNumbersOnly}`, JSON.stringify(data))
-        
-        if(document.getElementById(todayNumbersOnly)) {
-            document.getElementById(`${todayNumbersOnly}-weight`).innerText = `${weightInput.value}kg`
-            weighIns.pop()
-            weighIns.push(data)
+            if(document.getElementById(todayNumbersOnly)) {
+                document.getElementById(`${todayNumbersOnly}-weight`).innerText = `${data.weightInKg}kg`
+                weighIns.pop()
+                weighIns.push(data)
+            } else {
+                let tempTr = document.createElement('tr')
+                tempTr.innerHTML += `<td id="${todayNumbersOnly}">${JSON.parse(localStorage.getItem(todayNumbersOnly)).date}</td>
+                <td id="${todayNumbersOnly}-weight">${JSON.parse(localStorage.getItem(todayNumbersOnly)).weightInKg}kg</td>`
+                weighIns.push(data)
+                weightTable.children[0].after(tempTr)
+                    
+            }
+            renderAverages()
         } else {
-            let tempTr = document.createElement('tr')
-            tempTr.innerHTML += `<td id="${todayNumbersOnly}">${JSON.parse(localStorage.getItem(todayNumbersOnly)).date}</td>
-            <td id="${todayNumbersOnly}-weight">${JSON.parse(localStorage.getItem(todayNumbersOnly)).weightInKg}kg</td>`
-            weighIns.push(data)
-            weightTable.children[0].after(tempTr)
-                
+            let data = {
+                date: today,
+                weightInKg: (weightInput.value / 2.20462262185).toFixed(2),
+                weightInLbs: weightInput.value
+            }
+
+            localStorage.setItem(`${todayNumbersOnly}`, JSON.stringify(data))
+        
+            if(document.getElementById(todayNumbersOnly)) {
+                document.getElementById(`${todayNumbersOnly}-weight`).innerText = `${data.weightInLbs}lbs`
+                weighIns.pop()
+                weighIns.push(data)
+            } else {
+                let tempTr = document.createElement('tr')
+                tempTr.innerHTML += `<td id="${todayNumbersOnly}">${JSON.parse(localStorage.getItem(todayNumbersOnly)).date}</td>
+                <td id="${todayNumbersOnly}-weight">${JSON.parse(localStorage.getItem(todayNumbersOnly)).weightInLbs}lbs</td>`
+                weighIns.push(data)
+                weightTable.children[0].after(tempTr)
+                    
+            }
+            renderAverages()
         }
-        renderAverages()
+        
     }
+    weightInput.value = ''
+    suffixElement.classList.add('invisible')
 })
+
 
 function updateSuffix() {
     const width = getTextWidth(weightInput.value, '40px "Montserrat", sans-serif')
@@ -124,10 +195,18 @@ function updateSuffix() {
 
 function calculateAverage(arr) {
     let sum = 0
-    for(let i=0; i<arr.length; i++) {
-        sum += Number(arr[i].weightInKg)
-
+    if(isKg) {
+        for(let i=0; i<arr.length; i++) {
+            sum += Number(arr[i].weightInKg)
+    
+        }
+    } else {
+        for(let i=0; i<arr.length; i++) {
+            sum += Number(arr[i].weightInLbs)
+    
+        }
     }
+    
 
     return (sum/arr.length).toFixed(2)
 }
@@ -139,7 +218,8 @@ function renderAverages() {
 
     resetAveragesTable()
 
-    if((weighIns.length % 7) === 0) {
+    if(isKg) {
+        if((weighIns.length % 7) === 0) {
         for(let i=0; i < weighIns.length; i += 7) {
             tempArr = weighIns.slice(i, i+7)
             console.log(tempArr)
@@ -152,43 +232,96 @@ function renderAverages() {
         }
         averagesTable.innerHTML += tempHTML
 
-    } else if(weighIns.length > 7) {
-        let runTimes = parseInt(weighIns.length / 7)
-        let remainder = weighIns.length % 7
-        let count = 0
-        for(let i=0; i < runTimes; i++) {
-            tempArr = weighIns.slice(count, count+7)
+        } else if(weighIns.length > 7) {
+            let runTimes = parseInt(weighIns.length / 7)
+            let remainder = weighIns.length % 7
+            let count = 0
+            for(let i=0; i < runTimes; i++) {
+                tempArr = weighIns.slice(count, count+7)
+                averageWeight = calculateAverage(tempArr)
+                timeFrame = tempArr[0].date + " - " + tempArr[6].date
+                tempHTML = `<tr>
+                <td>${timeFrame}</td>
+                <td>${averageWeight}kg</td>
+                </tr>` + tempHTML
+                count += 7
+            }
+
+            tempArr = weighIns.slice((7 * runTimes), ((7 * runTimes) + remainder))
             averageWeight = calculateAverage(tempArr)
-            timeFrame = tempArr[0].date + " - " + tempArr[6].date
+            timeFrame = tempArr[0].date + " - " + tempArr[remainder - 1].date
             tempHTML = `<tr>
             <td>${timeFrame}</td>
             <td>${averageWeight}kg</td>
             </tr>` + tempHTML
-            count += 7
+
+            averagesTable.innerHTML += tempHTML
+        } else {
+            let runTimes = parseInt(weighIns.length / 7)
+            let remainder = weighIns.length % 7
+
+            tempArr = weighIns.slice((7 * runTimes), ((7 * runTimes) + remainder))
+            averageWeight = calculateAverage(tempArr)
+            timeFrame = tempArr[0].date + " - " + tempArr[remainder - 1].date
+            tempHTML = `<tr>
+            <td>${timeFrame}</td>
+            <td>${averageWeight}kg</td>
+            </tr>` + tempHTML
+
+            averagesTable.innerHTML += tempHTML
         }
-
-        tempArr = weighIns.slice((7 * runTimes), ((7 * runTimes) + remainder))
-        averageWeight = calculateAverage(tempArr)
-        timeFrame = tempArr[0].date + " - " + tempArr[remainder - 1].date
-        tempHTML = `<tr>
-        <td>${timeFrame}</td>
-        <td>${averageWeight}kg</td>
-        </tr>` + tempHTML
-
-        averagesTable.innerHTML += tempHTML
     } else {
-        let runTimes = parseInt(weighIns.length / 7)
-        let remainder = weighIns.length % 7
-
-        tempArr = weighIns.slice((7 * runTimes), ((7 * runTimes) + remainder))
-        averageWeight = calculateAverage(tempArr)
-        timeFrame = tempArr[0].date + " - " + tempArr[remainder - 1].date
-        tempHTML = `<tr>
-        <td>${timeFrame}</td>
-        <td>${averageWeight}kg</td>
-        </tr>` + tempHTML
-
+        if((weighIns.length % 7) === 0) {
+        for(let i=0; i < weighIns.length; i += 7) {
+            tempArr = weighIns.slice(i, i+7)
+            console.log(tempArr)
+            averageWeight = calculateAverage(tempArr)
+            timeFrame = tempArr[0].date + " - " + tempArr[6].date
+            tempHTML = `<tr>
+            <td>${timeFrame}</td>
+            <td>${averageWeight}lbs</td>
+            </tr>` + tempHTML
+        }
         averagesTable.innerHTML += tempHTML
+
+        } else if(weighIns.length > 7) {
+            let runTimes = parseInt(weighIns.length / 7)
+            let remainder = weighIns.length % 7
+            let count = 0
+            for(let i=0; i < runTimes; i++) {
+                tempArr = weighIns.slice(count, count+7)
+                averageWeight = calculateAverage(tempArr)
+                timeFrame = tempArr[0].date + " - " + tempArr[6].date
+                tempHTML = `<tr>
+                <td>${timeFrame}</td>
+                <td>${averageWeight}lbs</td>
+                </tr>` + tempHTML
+                count += 7
+            }
+
+            tempArr = weighIns.slice((7 * runTimes), ((7 * runTimes) + remainder))
+            averageWeight = calculateAverage(tempArr)
+            timeFrame = tempArr[0].date + " - " + tempArr[remainder - 1].date
+            tempHTML = `<tr>
+            <td>${timeFrame}</td>
+            <td>${averageWeight}lbs</td>
+            </tr>` + tempHTML
+
+            averagesTable.innerHTML += tempHTML
+        } else {
+            let runTimes = parseInt(weighIns.length / 7)
+            let remainder = weighIns.length % 7
+
+            tempArr = weighIns.slice((7 * runTimes), ((7 * runTimes) + remainder))
+            averageWeight = calculateAverage(tempArr)
+            timeFrame = tempArr[0].date + " - " + tempArr[remainder - 1].date
+            tempHTML = `<tr>
+            <td>${timeFrame}</td>
+            <td>${averageWeight}lbs</td>
+            </tr>` + tempHTML
+
+            averagesTable.innerHTML += tempHTML
+        }
     }
 }
 
@@ -198,6 +331,14 @@ function resetAveragesTable() {
     <th>Average</th>
     </tr>` 
 }
+
+function resetWeighInsTable() {
+    weightTable.innerHTML = `<tr>
+    <th>Date</th>
+    <th>Weight</th>
+    </tr>` 
+}
+
 
 function getTextWidth(text, font) {
     var canvas = getTextWidth.canvas || (getTextWidth.canvas = document.createElement("canvas"));
